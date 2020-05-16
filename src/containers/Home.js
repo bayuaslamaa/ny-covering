c8bf5a86f146313efb037fbef623d6cbe33ce5fa
bayuaslamaa <bayuaslamaa@gmail.com>
2020-05-16 22:49:05 +0700

    update styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,30 +7,76 @@ import { Carousel } from 'react-bootstrap'
 export default function Home() {
     const dispatch = useDispatch()
     const products = useSelector(state => state.products)
+    const khimar = useSelector(state => state.khimar)
+    const abaya = useSelector(state => state.abaya)
     useEffect(() => {
         dispatch(getProducts())
     }, [dispatch])
     return (
-        <div className="col-5 offset-4">
-            <Carousel interval={800}>
-                {
-                    products.map(product => (<Carousel.Item key={product.id}>
-                        <img
-                            style={{ height: "100%", width: "100%" }}
-                            className="d-block"
-                            src={product.image_url}
-                            alt="slide"
-                        />
-                        {/* <Carousel.Caption>
+        <div className="container" style={{ display: "flex", flexDirection: 'row', flexWrap: "wrap", justifyContent: "space-evenly" }}>
+            <div className="col-3 " >
+                <Carousel interval={800}>
+                    {
+                        products.map(product => (<Carousel.Item key={product.id}>
+                            <img
+                                style={{ height: "100%", width: "100%" }}
+                                className="d-block"
+                                src={product.image_url}
+                                alt="slide"
+                            />
+                            {/* <Carousel.Caption>
                             <h1>{product.name}</h1>
                             <h3>IDR {product.price}</h3>
                         </Carousel.Caption> */}
-                    </Carousel.Item>))
-                }
+                        </Carousel.Item>))
+                    }
 
 
 
-            </Carousel>
+                </Carousel>
+            </div>
+            <div className="col-3">
+                <Carousel interval={800}>
+                    {
+                        khimar.map(khim => (<Carousel.Item key={khim.id}>
+                            <img
+                                style={{ height: "100%", width: "100%" }}
+                                className="d-block"
+                                src={khim.image_url}
+                                alt="slide"
+                            />
+                            {/* <Carousel.Caption>
+                            <h1>{product.name}</h1>
+                            <h3>IDR {product.price}</h3>
+                        </Carousel.Caption> */}
+                        </Carousel.Item>))
+                    }
+
+
+
+                </Carousel>
+            </div>
+            <div className="col-3">
+                <Carousel interval={800}>
+                    {
+                        products.map(product => (<Carousel.Item key={product.id}>
+                            <img
+                                style={{ height: "100%", width: "100%" }}
+                                className="d-block"
+                                src={product.image_url}
+                                alt="slide"
+                            />
+                            {/* <Carousel.Caption>
+                            <h1>{product.name}</h1>
+                            <h3>IDR {product.price}</h3>
+                        </Carousel.Caption> */}
+                        </Carousel.Item>))
+                    }
+
+
+
+                </Carousel>
+            </div>
         </div>
     )
 }
