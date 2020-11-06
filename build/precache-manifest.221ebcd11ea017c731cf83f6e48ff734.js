e153893d216b8f9d79213cbebae452116a2fd6ad
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@Bayus-MacBook-Pro.local>
2020-11-06 18:04:08 +0700

    deployed

---
+++ build/precache-manifest.221ebcd11ea017c731cf83f6e48ff734.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea0b3eda71397b9ac74772020acc70a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17abf2c93ce3eff5ad24",
+    "url": "/static/css/2.6b0f9ba9.chunk.css"
+  },
+  {
+    "revision": "cc86f1ab6f6e0fb30096",
+    "url": "/static/css/main.f9f4735c.chunk.css"
+  },
+  {
+    "revision": "17abf2c93ce3eff5ad24",
+    "url": "/static/js/2.34f1e3a7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.34f1e3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc86f1ab6f6e0fb30096",
+    "url": "/static/js/main.b88b949b.chunk.js"
+  },
+  {
+    "revision": "3d250d420095f1e340d6",
+    "url": "/static/js/runtime-main.2e59cd40.js"
+  },
+  {
+    "revision": "8073069d2cf22e35c010cd7366c67a3a",
+    "url": "/static/media/AbayaDustyPink.8073069d.jpg"
+  },
+  {
+    "revision": "8a27add46722f500607d2cf9ca8fa9f3",
+    "url": "/static/media/AbayaMoccaNude.8a27add4.jpg"
+  },
+  {
+    "revision": "8e1a6c94cfdaa4b1e69afcaa52c5297e",
+    "url": "/static/media/AbayaNudeMocca.8e1a6c94.jpg"
+  },
+  {
+    "revision": "f766682968a28c2d540ca31f892612e4",
+    "url": "/static/media/BabyBlue.f7666829.jpg"
+  },
+  {
+    "revision": "6f21d1b71ef9907d8853933ed593fdf3",
+    "url": "/static/media/BabyBlueCadar.6f21d1b7.jpg"
+  },
+  {
+    "revision": "caf87883002757e7ddf7f1fda3dded99",
+    "url": "/static/media/BlackArmy.caf87883.jpg"
+  },
+  {
+    "revision": "1192ee348d9029fe258c4794309e4914",
+    "url": "/static/media/BlackBrownie.1192ee34.jpg"
+  },
+  {
+    "revision": "800e907c8f2d9288394ef1be1263c3e8",
+    "url": "/static/media/BlackDarkMustard.800e907c.jpg"
+  },
+  {
+    "revision": "6d17a25ea7dc1d6a5023416e72c669a0",
+    "url": "/static/media/BlackFossil.6d17a25e.jpg"
+  },
+  {
+    "revision": "6f819163a161ce8b7a5b7f009f03c508",
+    "url": "/static/media/BlackLilac.6f819163.jpg"
+  },
+  {
+    "revision": "51913856919f3a0688e73fd79a698da4",
+    "url": "/static/media/BlackMilky.51913856.jpg"
+  },
+  {
+    "revision": "7bd5c933d8d169574cf747a9f443f5a5",
+    "url": "/static/media/BlackMustard.7bd5c933.jpg"
+  },
+  {
+    "revision": "e464e481bf3c41994d9aac53e9a162fa",
+    "url": "/static/media/BlackOlive.e464e481.jpg"
+  },
+  {
+    "revision": "6b4cf2505c2ea5d487844d45524df95f",
+    "url": "/static/media/CandyPinkCadar.6b4cf250.jpg"
+  },
+  {
+    "revision": "b1ce94e212eb4edbeb8154095c0a9fa2",
+    "url": "/static/media/CarouselHome2.b1ce94e2.jpg"
+  },
+  {
+    "revision": "1ac2086fa4186b571e60c713959db1bc",
+    "url": "/static/media/CarouselHome3.1ac2086f.jpg"
+  },
+  {
+    "revision": "25b061399705b4208999705c37d80657",
+    "url": "/static/media/Carousell1.25b06139.jpeg"
+  },
+  {
+    "revision": "2fa2a9a9c111de39f070908fe81661a6",
+    "url": "/static/media/Carousell2.2fa2a9a9.jpeg"
+  },
+  {
+    "revision": "9ad842bbcd3684225087473f3a822084",
+    "url": "/static/media/Carousell3.9ad842bb.jpeg"
+  },
+  {
+    "revision": "e242ca835c40c11478f9d829e01cb761",
+    "url": "/static/media/CarouserHome1.e242ca83.jpg"
+  },
+  {
+    "revision": "5ab496556b3cb556da41bfe9451602bd",
+    "url": "/static/media/Coffee.5ab49655.jpg"
+  },
+  {
+    "revision": "ab6aff9f4aef3334f682fa959497065a",
+    "url": "/static/media/DustyPink2.ab6aff9f.jpg"
+  },
+  {
+    "revision": "59846faddb8a0145c755560dbb8c7307",
+    "url": "/static/media/LightTaupeCadar.59846fad.jpg"
+  },
+  {
+    "revision": "4082a2b1cc7c04c75b124ae65829b816",
+    "url": "/static/media/LovelyPink.4082a2b1.jpg"
+  },
+  {
+    "revision": "98a93f1ab5e28bfee5347dabcdcdb278",
+    "url": "/static/media/LovelyPink2.98a93f1a.jpg"
+  },
+  {
+    "revision": "cbd96ed643fb54ee822dddb68f06372a",
+    "url": "/static/media/Mocca.cbd96ed6.jpg"
+  },
+  {
+    "revision": "e980423b10b578ce07c58ff42776d77c",
+    "url": "/static/media/ModelOlive.e980423b.jpg"
+  },
+  {
+    "revision": "97eb30729bd3d3b06d188dfd30f19c14",
+    "url": "/static/media/ModelPink.97eb3072.jpg"
+  },
+  {
+    "revision": "f0e44d1411648518098d0eb2d3bbd87b",
+    "url": "/static/media/Navy.f0e44d14.jpg"
+  },
+  {
+    "revision": "73b16e5048fd1a16bb3a39b8f8b5ebdf",
+    "url": "/static/media/Nude.73b16e50.jpg"
+  },
+  {
+    "revision": "4a4ce6af18f08aeecb6bd7d442b553ef",
+    "url": "/static/media/OatVanilla.4a4ce6af.jpg"
+  },
+  {
+    "revision": "a6199fed09e7434d158eca924e7df532",
+    "url": "/static/media/OliveGreenCadar.a6199fed.jpg"
+  },
+  {
+    "revision": "934895ac1a386f40f8367a7665067fd8",
+    "url": "/static/media/PeachBeige.934895ac.jpeg"
+  },
+  {
+    "revision": "46b118dd716667dc910840cc6762ff7d",
+    "url": "/static/media/RugbyTan.46b118dd.jpeg"
+  },
+  {
+    "revision": "7cff704fd5a8239d9d65f798236353e9",
+    "url": "/static/media/SetMoccaNude.7cff704f.jpg"
+  },
+  {
+    "revision": "e1e1231d183f26459ae490fe4609d6ce",
+    "url": "/static/media/SkyBlue.e1e1231d.jpg"
+  },
+  {
+    "revision": "cffbec5734ac12dccfcb93833c0c4409",
+    "url": "/static/media/ZaraBrownie.cffbec57.jpg"
+  },
+  {
+    "revision": "6f1e05c538f8d3917b093d5b5c4cf065",
+    "url": "/static/media/ZaraFossil.6f1e05c5.jpg"
+  },
+  {
+    "revision": "fe3e8b50ef6801a800fe5743922dc9b7",
+    "url": "/static/media/ZaraLilac.fe3e8b50.jpg"
+  },
+  {
+    "revision": "1ee62e230b0f621a567b38e22eea6882",
+    "url": "/static/media/ZaraMilky.1ee62e23.jpg"
+  },
+  {
+    "revision": "d301c7051755cb21fec8465e81b6a1e6",
+    "url": "/static/media/ZaraOlive.d301c705.jpg"
+  },
+  {
+    "revision": "5cd37f94600e528ff621ef69c9e5b492",
+    "url": "/static/media/baked-clay.5cd37f94.jpeg"
+  },
+  {
+    "revision": "133b15319faa31b07ad79eeee7d05c17",
+    "url": "/static/media/blacky.133b1531.JPG"
+  },
+  {
+    "revision": "aa6d982619836fae2aecaec4fbefccd5",
+    "url": "/static/media/bottle-green.aa6d9826.jpeg"
+  },
+  {
+    "revision": "6cbdcfc2324a556486b1aa59a602d990",
+    "url": "/static/media/caramel.6cbdcfc2.jpeg"
+  },
+  {
+    "revision": "db2841d56534b79b34369482e4f5c2ab",
+    "url": "/static/media/coral-reef.db2841d5.jpeg"
+  },
+  {
+    "revision": "c86aedd56e7f9e46a3ab10dffcca66df",
+    "url": "/static/media/dusty-cedar.c86aedd5.jpeg"
+  },
+  {
+    "revision": "c4035a982bbda41290a34fe7e1416972",
+    "url": "/static/media/dusty.c4035a98.jpeg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "7c29deb19f789eea66f5cda795d78e6b",
+    "url": "/static/media/jade-lime.7c29deb1.jpeg"
+  },
+  {
+    "revision": "e4e59d4a2814a58281fb31b5134af198",
+    "url": "/static/media/kahla-model-a.e4e59d4a.jpg"
+  },
+  {
+    "revision": "6c2884c9083e9d1be279d360fe572116",
+    "url": "/static/media/kahla-model-b.6c2884c9.jpg"
+  },
+  {
+    "revision": "34ce061f335ebdea517893417f52e526",
+    "url": "/static/media/kahla-model-c.34ce061f.jpg"
+  },
+  {
+    "revision": "b0ac00e04de1a413d3ae8bb463e2a076",
+    "url": "/static/media/light-gray.b0ac00e0.jpeg"
+  },
+  {
+    "revision": "3264d55486abe609c7b305c164c8492a",
+    "url": "/static/media/lime-punch.3264d554.jpeg"
+  },
+  {
+    "revision": "5a6ada5dc0ab2879a5d92248c82703bc",
+    "url": "/static/media/logo.5a6ada5d.png"
+  },
+  {
+    "revision": "9e7b66b3885f32691c4dde067619aaac",
+    "url": "/static/media/midnight-blue.9e7b66b3.jpeg"
+  },
+  {
+    "revision": "10b708772724a24d5a9f3e35c99e969a",
+    "url": "/static/media/military-olive.10b70877.jpeg"
+  },
+  {
+    "revision": "8024945289335b01484016580fc03629",
+    "url": "/static/media/mocha-bisque.80249452.jpeg"
+  },
+  {
+    "revision": "f6c3b59e0cb724bce0ba02777ef956d9",
+    "url": "/static/media/nyco.f6c3b59e.png"
+  },
+  {
+    "revision": "451fc812c48df2049acc9b9a33ea60a1",
+    "url": "/static/media/pearled-ivory.451fc812.jpeg"
+  },
+  {
+    "revision": "7e540c5c944460f454b0733638731204",
+    "url": "/static/media/setDustyPink.7e540c5c.jpg"
+  },
+  {
+    "revision": "d2a79806605364bcd7bb0d2d8da05cfa",
+    "url": "/static/media/setNudeMocca.d2a79806.jpg"
+  },
+  {
+    "revision": "ae516d1c43e1c81189f39bc5ade53973",
+    "url": "/static/media/sudan-brown.ae516d1c.jpeg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e138567056eebea35639e40ffdea231.js"
+  "/precache-manifest.221ebcd11ea017c731cf83f6e48ff734.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
